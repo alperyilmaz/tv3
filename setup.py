f4517827aaad3c689f36806d4e2d339432affdd9
Alper Yilmaz <alperyilmaz@gmail.com>
2022-01-16 22:27:43 +0300

    upgrade urwid version

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ with open('README.md') as f_readme:
     readme_text = f_readme.read()
 
 setuptools.setup(
-    author='Aramís Concepción Durán',
+    author='Alper Yilmaz (forked from Aramís Concepción Durán)',
     description='A text-based note-taking application',
     entry_points='[console_scripts]\ntv3=terminal_velocity:main\n',
-    install_requires=['urwid==1.1.1'],
+    install_requires=['urwid==2.1.2'],
     license='GNU General Public License, Version 3',
     long_description=readme_text,
     name='tv3',
     package_dir={'': 'src'},
     py_modules=['notebook', 'terminal_velocity', 'urwid_ui'],
-    url='github.com/aramiscd/tv3',
+    url='github.com/alperyilmaz/tv3',
     version='0.1',
 )
